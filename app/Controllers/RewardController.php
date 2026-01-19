86d5af6d69f9fe6f83fa8fbe8ddfa03ccd1227f6
elfitah <elfitahchaimaa@gmail.com>
2026-01-19 12:20:09 +0100

    ajout de rawardController

---
+++ app/Controllers/RewardController.php
@@ -0,0 +1,24 @@
+<?php
+
+class RewardController{
+    private $twig;
+    private $rewardModel;
+
+    public function __construct($twig,$pdo)
+    {
+        $this->rewardModel = new Reward($pdo);
+        $this->twig = $twig;
+    }
+
+
+    public function index()
+    {
+        $rewards = $this->rewardModel->getAll();
+
+        echo $this->twig->render('rewards/index.twig', [
+            'rewards' => $rewards
+        ]);
+    }
+}
+
+?>
